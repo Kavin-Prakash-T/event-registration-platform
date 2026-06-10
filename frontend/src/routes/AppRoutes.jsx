0aba35571767be3c5cb0182285d31ca7a3b29049
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2026-06-10 22:47:33 +0530

    feat: add participant routes for dashboard, events, event details, and registrations with protected access

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,10 @@ import Signup from "../pages/auth/Signup";
 import VerifyEmail from "../pages/auth/VerifyEmail";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
+import ParticipantDashboard from "../pages/participant/ParticipantDashboard";
+import Events from "../pages/participant/Events";
+import EventDetails from "../pages/participant/EventDetails";
+import MyRegistrations from "../pages/participant/MyRegistrations";
 
 import ProtectedRoute from "../components/ProtectedRoute";
 
@@ -36,7 +40,43 @@ const AppRoutes = () => {
           </ProtectedRoute>
         }
       />
+      <Route
+        path="/participant/dashboard"
+        element={
+          <ProtectedRoute role="PARTICIPANT">
+            <ParticipantDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/participant/events"
+        element={
+          <ProtectedRoute role="PARTICIPANT">
+            <Events />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/participant/events/:id"
+        element={
+          <ProtectedRoute role="PARTICIPANT">
+            <EventDetails />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/participant/registrations"
+        element={
+          <ProtectedRoute role="PARTICIPANT">
+            <MyRegistrations />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
+
   );
 };
 
